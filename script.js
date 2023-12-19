52230c5f4b99bc35e04dea7e38ee69c381921081
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2023-12-19 17:11:53 -0300

    Building play-pause.

---
+++ script.js
@@ -0,0 +1,18 @@
+const stopwatch = document.getElementById('stopwatch');
+const playPauseButton = document.getElementById('playPause');
+const secondsSphere = document.getElementById('seconds-sphere');
+
+let stopwatchInterval;
+let runningTime = 0;
+
+const playPause = () => {
+    const isPaused = !playPauseButton.classList.contains('running');
+    if(isPaused) {
+        playPauseButton.classList.add('running');
+        start();
+    } else {
+        playPauseButton.classList.remove('running');
+        pause();
+    }
+
+}
