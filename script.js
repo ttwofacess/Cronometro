366a34025f159259c94f9b728869de802d3cc6c3
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2023-12-20 14:54:12 -0300

    Completando.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const stopwatch = document.getElementById('stopwatch');
-const playPauseButton = document.getElementById('playPause');
+const playPauseButton = document.getElementById('play-pause');
 const secondsSphere = document.getElementById('seconds-sphere');
 
 let stopwatchInterval;
@@ -14,5 +14,38 @@ const playPause = () => {
         playPauseButton.classList.remove('running');
         pause();
     }
+}
 
+const pause = () => {
+    secondsSphere.style.animationPlayState = 'paused';
+    clearInterval(stopwatchInterval);
+}
+
+const stop = () => {
+    secondsSphere.style.transform = 'rotate(-90deg) translateX(60px)';
+    secondsSphere.style.animation = 'none';
+    playPauseButton.classList.remove('running');
+    runningTime = 0;
+    clearInterval(stopwatchInterval);
+    stopwatch.textContent = '00:00';
+}
+
+const start = () => {
+    secondsSphere.style.animation = 'rotacion 60s linear infinite';
+    let startTime = Date.now() - runningTime;
+    secondsSphere.style.animationPlayState = 'running';
+    stopwatchInterval = setInterval(() => {
+        runningTime = Date.now() - startTime;
+        stopwatch.textContent = calculateTime(runningTime);
+    }, 1000)
+}
+
+const calculateTime = runningTime => {
+    const totalSeconds = Math.floor(runningTime / 1000);
+    const totalMinutes = Math.floor(totalSeconds / 60);
+
+    const display_seconds = (totalSeconds % 60).toString().padStart(2, '0');
+    const display_minutes = totalMinutes.toString().padStart(2, '0'); 
+
+    return `${display_minutes}:${display_seconds}`;
 }
